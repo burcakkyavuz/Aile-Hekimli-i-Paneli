58db73d4e7f13a4a5d0c3d525a86a8ae72e5c443
burcakkyavuz <burcakkyavuz@gmail.com>
2025-12-11 16:02:49 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,16 @@
-const CACHE_NAME = "aile-hekimligi-cache-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.svg",
-  "./icon-512.svg"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("ahp-cache-v1").then(cache => cache.addAll([
+      "./",
+      "./index.html",
+      "./manifest.json",
+      "./icon-192.svg",
+      "./icon-512.svg"
+    ]))
   );
 });
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
-  );
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((res) => res || fetch(event.request))
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
